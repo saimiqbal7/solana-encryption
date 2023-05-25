60cff87b8345017cf223bea79aa2ab7e5726a693
saim <saim@koii.network>
2023-05-25 11:45:24 -0300

    updated files

--- README.md
+++ README.md
@@ -14,26 +14,21 @@ yarn add solana-encryption
 
 ```js
 const { encrypt, decrypt, nonce } = require("solana-encryption");
+const Keypair = require('@solana/web3.js').Keypair;
 
 const message = "Hello World";
-const publicKey_sender = "4VRqZyNrGUpApvgXAV39B342qdHTq9PGS2WbT55XzeaA";
-const privateKey_sender = new Uint8Array([  
-  132, 74, 129, 68, 65, 80, 64, 127, 108, 40, 189, 220, 216, 232, 242, 66,
-  61, 156, 83, 28, 10, 14, 47, 249, 132, 229, 232, 201, 248, 73, 89, 152,
-  51, 219, 104, 59, 111, 32, 190, 25, 154, 141, 42, 177, 240, 132, 71,
-  138, 87, 151, 35, 81, 74, 93, 169, 249, 188, 36, 23, 55, 53, 130, 24,
-  255,
-]);
 
-const publicKey_receiver = "DE9Bj5ZvuGaKzszMNH9kNZFUTtcsS61zkQk1osFCrD9R";
-const privateKey_receiver = new Uint8Array([
-  178, 119, 119, 186, 189, 96, 234, 41, 118, 99, 34, 21, 5, 236, 129, 96,
-  236, 182, 201, 0, 240, 23, 63, 251, 17, 210, 203, 123, 112, 141, 96,
-  231, 181, 170, 12, 97, 4, 250, 99, 214, 92, 206, 137, 92, 57, 220, 203,
-  160, 122, 135, 126, 89, 168, 120, 211, 143, 116, 178, 56, 63, 251, 185,
-  61, 48,
-]);
+// Generate two random key pairs
+const keypairA = Keypair.generate();
+const keypairB = Keypair.generate();
 
+// Get the public and private keys from the keypairs
+const publicKey_sender = keypairA.publicKey.toBase58();
+const privateKey_sender = keypairA.secretKey;
+const publicKey_receiver = keypairB.publicKey.toBase58();
+const privateKey_receiver = keypairB.secretKey;
+
+// Generate a nonce
 const newNonce = nonce();
 
 const encrypted = encrypt(
--- tests/index.js
+++ tests/index.js
@@ -1,34 +1,36 @@
-const {encrypt, decrypt} = require('../lib/encryptDecrypt');
-const nacl = require('tweetnacl');
-const ed2curve = require('ed2curve');
+const { encrypt, decrypt, nonce } = require("solana-encryption");
 const Keypair = require('@solana/web3.js').Keypair;
 
+const message = "Hello World";
 
-    //RANDOM SOLANA KEYS
-     const publicKeyA = "4VRqZyNrGUpApvgXAV39B342qdHTq9PGS2WbT55XzeaA";
-      const privateKeyA = new Uint8Array([
-        132, 74, 129, 68, 65, 80, 64, 127, 108, 40, 189, 220, 216, 232, 242, 66,
-        61, 156, 83, 28, 10, 14, 47, 249, 132, 229, 232, 201, 248, 73, 89, 152,
-        51, 219, 104, 59, 111, 32, 190, 25, 154, 141, 42, 177, 240, 132, 71,
-        138, 87, 151, 35, 81, 74, 93, 169, 249, 188, 36, 23, 55, 53, 130, 24,
-        255,
-      ]);
-      const publicKeyB = "DE9Bj5ZvuGaKzszMNH9kNZFUTtcsS61zkQk1osFCrD9R";
-      const privateKeyB = new Uint8Array([
-        178, 119, 119, 186, 189, 96, 234, 41, 118, 99, 34, 21, 5, 236, 129, 96,
-        236, 182, 201, 0, 240, 23, 63, 251, 17, 210, 203, 123, 112, 141, 96,
-        231, 181, 170, 12, 97, 4, 250, 99, 214, 92, 206, 137, 92, 57, 220, 203,
-        160, 122, 135, 126, 89, 168, 120, 211, 143, 116, 178, 56, 63, 251, 185,
-        61, 48,
-      ]);
+// Generate two random key pairs
+const keypairA = Keypair.generate();
+const keypairB = Keypair.generate();
 
-const message = 'Hello Saim!'; 
-console.log('message: ', message);
-const nonce = nacl.randomBytes(nacl.box.nonceLength);
-console.log('nonce: ', nonce);
+// Get the public and private keys from the keypairs
+const publicKey_sender = keypairA.publicKey.toBase58();
+const privateKey_sender = keypairA.secretKey;
+const publicKey_receiver = keypairB.publicKey.toBase58();
+const privateKey_receiver = keypairB.secretKey;
 
-const encrypted = encrypt(message, nonce, publicKeyB, privateKeyA);
-console.log('encrypted: ', encrypted);
+// Generate a nonce
+const newNonce = nonce();
 
-const decrypted = decrypt(encrypted, nonce, publicKeyA, privateKeyB);
-console.log('decrypted: ', decrypted);
+const encrypted = encrypt(
+  message,
+  newNonce,
+  publicKey_receiver,
+  privateKey_sender
+);
+
+console.log("encrypted: ", encrypted);
+
+const decrypted = decrypt(
+  encrypted,
+  newNonce,
+  publicKey_sender,
+  privateKey_receiver
+
+);
+
+console.log("decrypted: ", decrypted);
